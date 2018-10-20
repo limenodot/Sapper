54b8ddf7cae5a248df8b21efd766872173323456
Alexey <43588870+motherrhacker@users.noreply.github.com>
2018-10-20 23:02:05 +0300

    Add files via upload
    Все идеи в комментарии в основном файле

--- src/Sapper/ButtonMine.java
+++ src/Sapper/ButtonMine.java
@@ -1,7 +1,20 @@
-package Sapper;
+package application;
 
+import java.awt.*;
 import javafx.scene.control.Button;
 
-public class ButtonMine extends Button{
+public class ButtonMine extends Button {
+    protected boolean isMined;
 
+    public ButtonMine(boolean isMined) throws HeadlessException {
+        this.isMined = isMined;
+    }
+
+    public boolean isMined() {
+        return isMined;
+    }
+
+    public void setMined(boolean mined) {
+        isMined = mined;
+    }
 }
