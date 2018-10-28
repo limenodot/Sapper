18d00208cbee6ededacf9ac39aaa6b7f62f8a6c5
GravisUltrasound <43550468+GravisUltrasound@users.noreply.github.com>
2018-10-28 21:33:48 +0300

    Add files via upload

--- src/Sapper/ButtonMine.java
+++ src/Sapper/ButtonMine.java
@@ -1,7 +1,161 @@
 package Sapper;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ButtonMine extends Button{
+import static Sapper.Main.X_CELLS;
+import static Sapper.Main.Y_CELLS;
+
+public class ButtonMine extends Button {
+    protected int xPos;
+    protected int yPos;
+    protected boolean isMined;
+    protected boolean isActivated;
+    protected boolean isFlagged;
+    protected boolean isAsked;
+    protected int numberOfBombsAround;
+
+    public ButtonMine(int xPos, int yPos, boolean isMined, boolean isActivated, boolean isFlagged, int bombsAround) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.isMined = isMined;
+        this.isActivated = isActivated;
+        this.isFlagged = isFlagged;
+        this.numberOfBombsAround = bombsAround;
+        this.isAsked = isAsked;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public boolean isMined() {
+        return isMined;
+    }
+
+    public void setMined(boolean mined) {
+        isMined = mined;
+    }
+
+    public boolean isActivated() {
+        return isActivated;
+    }
+
+    public void setActivated(boolean activated) {
+        isActivated = activated;
+    }
+
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+    public void setFlagged(boolean flagged) {
+        isFlagged = flagged;
+    }
+    public boolean isAsked() {
+        return isAsked;
+    }
+
+    public void setAsked(boolean asked) {
+        isAsked = asked;
+    }
+
+    public int getNumberOfBombsAround() {
+        return numberOfBombsAround;
+    }
+
+    public void setNumberOfBombsAround(int bombsAround) {
+        this.numberOfBombsAround = bombsAround;
+    }
+
+
+
+    public List<ButtonMine> getNeighbours(ButtonMine button, ButtonMine[][] minefield) {
+        List<ButtonMine> neighbours = new ArrayList<>();
+
+        int[] points = new int[]{
+                -1, -1,
+                -1, 0,
+                -1, 1,
+                0, -1,
+                0, 1,
+                1, 1,
+                1, 0,
+                1, -1
+        };
+
+        for (int i = 0; i < points.length; i++) {
+            int dx = points[i];
+            int dy = points[++i];
+            int newX = this.xPos + dx;
+            int newY = this.yPos + dy;
+
+            if (newX >= 0 && newX < X_CELLS
+                    && newY >= 0 && newY < Y_CELLS) {
+                neighbours.add(minefield[newX][newY]);
+            }
+        }
+
+        return neighbours;
+    }
+    public void press(ButtonMine[][] minefield) {
+        if (this.getNumberOfBombsAround() != 0) {
+            String path = String.format("/Pics/%s.png", this.getNumberOfBombsAround());
+            InputStream input = getClass().getResourceAsStream(path);
+            Image image = new Image(input);
+            ImageView imageView = new ImageView(image);
+            this.setStyle("-fx-background-color: white; -fx-border-color: grey");
+            this.setGraphic(imageView);
+            this.setActivated(true);
+        } else {
+
+            this.setStyle("-fx-background-color: white; -fx-border-color: grey");
+            int[] points = new int[]{
+                    -1, -1,
+                    -1, 0,
+                    -1, 1,
+                    0, -1,
+                    0, 1,
+                    1, 1,
+                    1, 0,
+                    1, -1
+            };
+            for (int i = 0; i < points.length; i++) {
+                int dx = points[i];
+                int dy = points[++i];
+                int newX = this.xPos + dx;
+                int newY = this.yPos + dy;
+                if (newX >= 0 && newX < X_CELLS && newY >= 0 && newY < Y_CELLS) {
+                    if (minefield[newX][newY].isActivated == false) {
+                        try {
+                            minefield[newX][newY].press(minefield);
+                        } catch (ArrayIndexOutOfBoundsException aiobe) {
+                            System.out.println(" ");
+                        }
+                    }
+                }
+                this.setActivated(true);
+            }
+
+            return;
+        }
+    }
+
 
 }
--- src/Sapper/Main.java
+++ src/Sapper/Main.java
@@ -1,51 +1,225 @@
 package Sapper;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.RowConstraints;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
 import java.io.InputStream;
 
-public class Main extends Application{
+import static javafx.scene.input.MouseButton.PRIMARY;
 
-    public static void main(String[] args) {
+public class Main extends Application {
 
-        Application.launch(args);
-    }
-    int SIZE = 50;
-    int length = SIZE;
-    int width = SIZE;
+    public static final int WINDOW_WIDTH = 590;
+    public static final int WINDOW_HIGHT = 715;
+    public static final int X_CELLS = 10;
+    public static final int Y_CELLS = 10;
+    public static final double RAND_CONST = 0.20;
+    public static int BOMBS_LEFT = 0;
+    public static int BOMBS_VIEW = 0;
+
+    public ButtonMine[][] minefield = new ButtonMine[X_CELLS][Y_CELLS];
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) throws Exception {
 
         GridPane root = new GridPane();
-        Scene scene = new Scene(root, 500, 500);
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HIGHT);
 
-        for(int y = 0; y < length; y++) {
-
-            for (int x = 0; x < width; x++) {
-                Button button = new Button("");
+        for (int x = 0; x < X_CELLS; x++)
+            for (int y = 0; y < Y_CELLS; y++) {
+                ButtonMine button = new ButtonMine(x, y, Math.random() < RAND_CONST, false, false, 0);
                 button.setMaxWidth(Double.MAX_VALUE);
                 button.setMaxHeight(Double.MAX_VALUE);
-                InputStream input =  getClass().getResourceAsStream("/Pics/boom.png");
-                Image image = new Image(input);
-                ImageView imageView = new ImageView(image);
-                button.setGraphic(imageView);
-                root.getColumnConstraints().add(new ColumnConstraints(50));
-                root.getRowConstraints().add(new RowConstraints(50));
+                button.setStyle("-fx-background-color: lightgrey");
+                button.setStyle("-fx-border-color: grey");
+                minefield[x][y] = button;
+                root.getColumnConstraints().add(new ColumnConstraints(60));
+                root.getRowConstraints().add(new RowConstraints(65));
                 root.add(button, x, y);
             }
+        root.getRowConstraints().add(new RowConstraints(65));
 
-        }
-        stage.setScene(scene);
-        root.setGridLinesVisible(true);
 
-        stage.show();
+        for (int i = 0; i < X_CELLS; i++)
+            for (int j = 0; j < Y_CELLS; j++) {
+                ButtonMine buttonMine = minefield[i][j];
+                if (buttonMine.isMined() == true) {
+                    ++BOMBS_LEFT;
+                    ++BOMBS_VIEW;
+                }
+            }
+
+        System.out.print(BOMBS_LEFT);
+        String bt;
+        bt = String.format("%d", BOMBS_VIEW);
+        Label label = new Label(String.format(bt));
+        root.add(label, 8, 10);
+
+        for (int x = 0; x < X_CELLS; x++)
+            for (int y = 0; y < Y_CELLS; y++) {
+                ButtonMine button = minefield[x][y];
+                long bombs = button.getNeighbours(button, minefield).stream().filter(t -> t.isMined).count();
+                button.setNumberOfBombsAround((int) bombs);
+            }
+
+        for (int x = 0; x < X_CELLS; x++)
+            for (int y = 0; y < Y_CELLS; y++) {
+                ButtonMine button = minefield[x][y];
+                button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        MouseButton mouseButton = event.getButton();
+                        if (mouseButton == PRIMARY) {
+                            if (button.isMined() == true) {
+                                InputStream input = getClass().getResourceAsStream("/Pics/boom.png");
+                                Image image = new Image(input);
+                                ImageView imageView = new ImageView(image);
+                                button.setGraphic(imageView);
+                                button.setStyle("-fx-background-color: white; -fx-border-color: grey");
+                                button.setActivated(true);
+                                gameover(primaryStage);
+                            } else {
+                                button.press(minefield);
+                            }
+                        } else {
+                            if (button.isActivated() == false) {
+                                if (button.isFlagged() == false && button.isAsked() == false) {
+                                    InputStream input = getClass().getResourceAsStream("/Pics/flag.png");
+                                    Image image = new Image(input);
+                                    ImageView imageView = new ImageView(image);
+                                    button.setGraphic(imageView);
+                                    button.setFlagged(true);
+                                    --BOMBS_VIEW;
+                                    String bt;
+                                    bt = String.format("%d", BOMBS_VIEW);
+                                    label.setText(bt);
+                                    if (button.isMined() == true) {
+                                        --BOMBS_LEFT;
+                                    } else {
+                                        ++BOMBS_LEFT;
+                                    }
+                                    if (BOMBS_LEFT == 0) {
+                                        youwin(primaryStage);
+                                    }
+                                } else {
+                                    if (button.isFlagged() == true && button.isAsked() == false) {
+                                        InputStream input = getClass().getResourceAsStream("/Pics/ask.png");
+                                        Image image = new Image(input);
+                                        ImageView imageView = new ImageView(image);
+                                        button.setGraphic(imageView);
+                                        button.setFlagged(false);
+                                        button.setAsked(true);
+                                        ++BOMBS_VIEW;
+                                        String bt;
+                                        bt = String.format("%d", BOMBS_VIEW);
+                                        label.setText(bt);
+                                        if (button.isMined() == true) {
+                                            ++BOMBS_LEFT;
+                                        } else {
+                                            --BOMBS_LEFT;
+                                        }
+                                        if (BOMBS_LEFT == 0) {
+                                            youwin(primaryStage);
+                                        }
+                                    } else {
+                                        InputStream input = getClass().getResourceAsStream("/Pics/empty.png");
+                                        Image image = new Image(input);
+                                        ImageView imageView = new ImageView(image);
+                                        button.setGraphic(imageView);
+                                        button.setFlagged(false);
+                                        button.setAsked(false);
+                                    }
+                                }
+                            } else {
+                                if (button.isActivated() == true && button.isFlagged() == true) {
+                                    InputStream input = getClass().getResourceAsStream("/Pics/empty.png");
+                                    ++BOMBS_VIEW;
+                                    String bt;
+                                    bt = String.format("%d", BOMBS_VIEW);
+                                    label.setText(bt);
+                                    Image image = new Image(input);
+                                    ImageView imageView = new ImageView(image);
+                                    button.setGraphic(imageView);
+                                    button.setFlagged(false);
+                                    button.setAsked(false);
+                                } else {
+                                    if (button.isAsked() == true) {
+                                        InputStream input = getClass().getResourceAsStream("/Pics/empty.png");
+                                        Image image = new Image(input);
+                                        ImageView imageView = new ImageView(image);
+                                        button.setGraphic(imageView);
+                                        button.setFlagged(false);
+                                        button.setAsked(false);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Sapper");
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
+
+    public void gameover(Stage primaryStage) {
+        for (int i = 0; i < X_CELLS; i++)
+            for (int j = 0; j < Y_CELLS; j++) {
+                ButtonMine buttonMine = minefield[i][j];
+                if (buttonMine.isMined() && buttonMine.isActivated() == false) {
+                    Image image1 = new Image("/Pics/bomb.png");
+                    ImageView imageView1 = new ImageView(image1);
+                    buttonMine.setGraphic(imageView1);
+                    buttonMine.setStyle("-fx-background-color: white; -fx-border-color: grey");
+                }
+            }
+
+        Label secondLabel = new Label("Game over :(");
+        StackPane secondaryLayout = new StackPane();
+        secondaryLayout.getChildren().add(secondLabel);
+        Scene secondScene = new Scene(secondaryLayout, 230, 100);
+        Stage secondaryStage = new Stage();
+        secondaryStage.initModality(Modality.WINDOW_MODAL);
+        secondaryStage.initOwner(primaryStage);
+        secondaryStage.setTitle("Sapper");
+        secondaryStage.setScene(secondScene);
+        secondaryStage.show();
+
+        secondaryStage.setOnCloseRequest(e -> Platform.exit());
+    }
+
+    public void youwin(Stage primaryStage) {
+        Label secondLabel = new Label("You win!!!\nCongratulations!");
+        StackPane secondaryLayout = new StackPane();
+        secondaryLayout.getChildren().add(secondLabel);
+        Scene secondScene = new Scene(secondaryLayout, 230, 100);
+        Stage secondaryStage = new Stage();
+        secondaryStage.initModality(Modality.WINDOW_MODAL);
+        secondaryStage.initOwner(primaryStage);
+        secondaryStage.setTitle("Sapper");
+        secondaryStage.setScene(secondScene);
+        secondaryStage.show();
+
+        secondaryStage.setOnCloseRequest(e -> Platform.exit());
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
