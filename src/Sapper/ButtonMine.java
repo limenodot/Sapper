83078e35598ed4c0fa3346e1e7ba144bc7094309
Alexey <43588870+motherrhacker@users.noreply.github.com>
2018-10-28 23:05:43 +0300

    Add files via upload

--- src/Sapper/ButtonMine.java
+++ src/Sapper/ButtonMine.java
@@ -1,15 +1,14 @@
-package application;
+package Sapper;
 
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import static application.SapperApplication.X_CELLS;
-import static application.SapperApplication.Y_CELLS;
+import static Sapper.Main.X_CELLS;
+import static Sapper.Main.Y_CELLS;
 
 public class ButtonMine extends Button {
     protected int xPos;
@@ -17,17 +16,17 @@ public class ButtonMine extends Button {
     protected boolean isMined;
     protected boolean isActivated;
     protected boolean isFlagged;
-    protected int bombsAround;
-    protected List<ButtonMine> neighbours;
+    protected boolean isAsked;
+    protected int numberOfBombsAround;
 
-    public ButtonMine(int xPos, int yPos, boolean isMined, boolean isActivated, boolean isFlagged, int bombsAround, List<ButtonMine> neighbours) {
+    public ButtonMine(int xPos, int yPos, boolean isMined, boolean isActivated, boolean isFlagged, int bombsAround) {
         this.xPos = xPos;
         this.yPos = yPos;
         this.isMined = isMined;
         this.isActivated = isActivated;
         this.isFlagged = isFlagged;
-        this.bombsAround = bombsAround;
-        this.neighbours = neighbours;
+        this.numberOfBombsAround = bombsAround;
+        this.isAsked = isAsked;
     }
 
     public int getxPos() {
@@ -69,28 +68,25 @@ public class ButtonMine extends Button {
     public void setFlagged(boolean flagged) {
         isFlagged = flagged;
     }
-
-    public int getBombsAround() {
-        return bombsAround;
+    public boolean isAsked() {
+        return isAsked;
     }
 
-    public List<ButtonMine> getNeighbours() {
-        return neighbours;
-    }
-
-    public void setNeighbours(List<ButtonMine> neighbours) {
-        this.neighbours = neighbours;
+    public void setAsked(boolean asked) {
+        isAsked = asked;
     }
 
     public int getNumberOfBombsAround() {
-        return bombsAround;
+        return numberOfBombsAround;
     }
 
-    public void setBombsAround(int bombsAround) {
-        this.bombsAround = bombsAround;
+    public void setNumberOfBombsAround(int bombsAround) {
+        this.numberOfBombsAround = bombsAround;
     }
 
-    public List<ButtonMine> getBombsAround(ButtonMine[][] minefield) {
+
+
+    public List<ButtonMine> getNeighbours(ButtonMine button, ButtonMine[][] minefield) {
         List<ButtonMine> neighbours = new ArrayList<>();
 
         int[] points = new int[]{
@@ -118,7 +114,6 @@ public class ButtonMine extends Button {
 
         return neighbours;
     }
-
     public void press(ButtonMine[][] minefield) {
         if (this.getNumberOfBombsAround() != 0) {
             String path = String.format("/Pics/%s.png", this.getNumberOfBombsAround());
@@ -147,14 +142,12 @@ public class ButtonMine extends Button {
                 int newX = this.xPos + dx;
                 int newY = this.yPos + dy;
                 if (newX >= 0 && newX < X_CELLS && newY >= 0 && newY < Y_CELLS) {
-                    System.out.printf("Current position before: %d %d\n", newX, newY);
                     if (minefield[newX][newY].isActivated == false) {
                         try {
                             minefield[newX][newY].press(minefield);
                         } catch (ArrayIndexOutOfBoundsException aiobe) {
                             System.out.println(" ");
                         }
-                        System.out.printf("Current position after: %d %d\n", newX, newY);
                     }
                 }
                 this.setActivated(true);
@@ -163,4 +156,6 @@ public class ButtonMine extends Button {
             return;
         }
     }
+
+
 }
---
+++ src/Sapper/Cell.java
@@ -0,0 +1,13 @@
+package Sapper;
+
+public class Cell {
+    protected int x;
+    protected int y;
+    protected boolean isMined;
+
+    public Cell(int x, int y, boolean isMined) {
+        this.x = x;
+        this.y = y;
+        this.isMined = isMined;
+    }
+}
